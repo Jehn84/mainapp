73e646650a7b5b0c94a0fd3fa4e10749b344e703
Иванов Евгений Юрьевич <ivanov@erc.ur.ru>
2019-06-03 16:59:24 +0500

    4

--- indexmassi/views.py
+++ indexmassi/views.py
@@ -9,6 +9,9 @@ def isint(s):
         return False
 
 
+def im(weight, height):
+    return round(int(weight)/(int(height)*int(height)/10000), 1)
+
 def weight_index(request):
     if request.method == "POST":
         weight = request.POST.get("weight")
@@ -22,32 +25,33 @@ def weight_index(request):
         elif 300 < int(height) or 100 > int(height) or 500 < int(weight) or int(weight) < 20:
             description = "Диапазон неверный!"
             return render(request, 'htmls/index.html', {"pic": "static/img/0.png", "description": description})
-        im = round(int(weight)/(int(height)*int(height)/10000), 1)
-        if 1 <= im <= 16:
+        # im = round(int(weight)/(int(height)*int(height)/10000), 1)
+        im(weight, height)
+        if 1 <= im(weight, height) <= 16:
             description = "Выраженный дефицит массы"
             pic = "static/img/1.png"
-        elif 16 < im <= 18.5:
+        elif 16 < im(weight, height) <= 18.5:
             description = "Недостаточная масса тела"
             pic = "static/img/1.png"
-        elif 18.5 < im <= 25:
+        elif 18.5 < im(weight, height) <= 25:
             description = "Норма"
             pic = "static/img/2.png"
-        elif 25 < im <= 30:
+        elif 25 < im(weight, height) <= 30:
             description = "Избыточная масса тела (предожирение)"
             pic = "static/img/3.png"
-        elif 30 < im <= 35:
+        elif 30 < im(weight, height) <= 35:
             description = "Ожирение первой степени!"
             pic = "static/img/4.png"
-        elif 35 < im < 40:
+        elif 35 < im(weight, height) < 40:
             description = "Ожирение второй степени!"
             pic = "static/img/5.png"
-        elif 500 > im >= 40:
+        elif 500 > im(weight, height) >= 40:
             description = "Ожирение третьей степени!"
             pic = "static/img/6.png"
         else:
             description = "Получен некорректный ИМТ!"
             pic = "static/img/0.png"
-        return render(request, 'htmls/index.html', {"message": format(im), 
+        return render(request, 'htmls/index.html', {"message": format(im(weight, height)),
                                                          "your_index_description": "Ваш индекс массы тела = ", 
                                                          "description": description, "pic": pic, "weight": weight, 
                                                          "height": height})
