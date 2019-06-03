86412a0e33823f509837c0486769de49814abcd6
Иванов Евгений Юрьевич <ivanov@erc.ur.ru>
2019-06-03 15:09:06 +0500

    2

--- indexmassi/views.py
+++ indexmassi/views.py
@@ -14,40 +14,40 @@ def indexmassi(request):
         ves = request.POST.get("ves")
         rost = request.POST.get("rost")
         if not isint(ves) or not isint(rost):
-            opisanie = "Ты ввёл вообще не цифры!"
-            return render(request, 'indexmassi/index.html', {"pic": "static/img/0.png", "opisanie": opisanie})
+            description = "Ты ввёл вообще не цифры!"
+            return render(request, 'indexmassi/index.html', {"pic": "static/img/0.png", "description": description})
         elif int(rost) == 0 or (300 < int(rost) < 100) or (500 < int(ves) < 20):
-            opisanie = "Делишь на ноль?"
-            return render(request, 'indexmassi/index.html', {"pic": "static/img/0.png", "opisanie": opisanie})
+            description = "Делишь на ноль?"
+            return render(request, 'indexmassi/index.html', {"pic": "static/img/0.png", "description": description})
         elif 300 < int(rost) or 100 > int(rost) or 500 < int(ves) or int(ves) < 20:
-            opisanie = "Диапазон неверный!"
-            return render(request, 'indexmassi/index.html', {"pic": "static/img/0.png", "opisanie": opisanie})
+            description = "Диапазон неверный!"
+            return render(request, 'indexmassi/index.html', {"pic": "static/img/0.png", "description": description})
         im = round(int(ves)/((int(rost) * int(rost) / 10000)), 1)
         if 1 <= im <= 16:
-            opisanie = "Вы дистрофан! Выраженный дефицит массы"
+            description = "Выраженный дефицит массы"
             pic = "static/img/1.png"
         elif 16 < im <= 18.5:
-            opisanie = "Вы тощий! Недостаточная масса тела"
+            description = "Недостаточная масса тела"
             pic = "static/img/1.png"
         elif 18.5 < im <= 25:
-            opisanie = "Ура! Вы нормальный"
+            description = "Норма"
             pic = "static/img/2.png"
         elif 25 < im <= 30:
-            opisanie = "Вы жирноваты! Избыточная масса тела (предожирение)"
+            description = "Избыточная масса тела (предожирение)"
             pic = "static/img/3.png"
         elif 30 < im <= 35:
-            opisanie = "Вы жирный! Ожирение 1-ой степени!"
+            description = "Ожирение первой степени!"
             pic = "static/img/4.png"
         elif 35 < im < 40:
-            opisanie = "Вы очень жирный! Ожирение 2-ой степени!"
+            description = "Ожирение второй степени!"
             pic = "static/img/5.png"
         elif 500 > im >= 40:
-            opisanie = "Вы супержирный! Ожирение 3-ей степени!"
+            description = "Ожирение третьей степени!"
             pic = "static/img/6.png"
         else:
-            opisanie = "Получен некорректный ИМТ!"
+            description = "Получен некорректный ИМТ!"
             pic = "static/img/0.png"
-        return render(request, 'indexmassi/index.html', {"message": format(im), "vashindexraventext": "Ваш индекс массы тела = ", "opisanie": opisanie, "pic": pic, "ves": ves, "rost": rost})
+        return render(request, 'indexmassi/index.html', {"message": format(im), "vashindexraventext": "Ваш индекс массы тела = ", "description": description, "pic": pic, "ves": ves, "rost": rost})
     pic = "static/img/0.png"
     return render(request, 'indexmassi/index.html',  {"pic": pic})
 
